dc308d0210224611f1d2df3c659e3bfd580da9e3
Gal Parselani <Parselinho@gmail.com>
2023-04-21 16:29:29 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,36 @@ var express = require('express');
 var router = express.Router();
 const { Book } = require('../models'); // Import Book model
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
+/* Redirect home page to /books */
+router.get('/', function(req, res, next) {
+  res.redirect('/books');
+});
+
+/* GET /books */
+router.get('/books', async function(req, res, next) {
   try {
     const books = await Book.findAll();
-    console.log(books);
+    // console.log(books);
     res.json(books);
   } catch (error) {
     next(error);
   }
 });
+
+/* GET /books/new - Show the create new book form */
+router.get('/books/new', function(req, res, next) {
+  res.render('new-book');
+});
+
+/* POST /books/new - Post a new book to the database */
+router.post('/books/new', async function(req, res, next) {
+  try {
+    const newBook = await Book.create(req.body);
+    res.redirect('/books');
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 module.exports = router;
