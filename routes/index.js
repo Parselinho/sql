37ebab58d4ad21e384dfd6cdaf214689135987af
Gal Parselani <Parselinho@gmail.com>
2023-04-23 20:33:58 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { Book } = require('../models'); // Import Book model
+const { Op } = require("sequelize");
 
 /* Redirect home page to /books */
 router.get('/', function(req, res, next) {
@@ -12,7 +13,8 @@ router.get('/books', async function(req, res, next) {
   try {
     const books = await Book.findAll();
     // console.log(books);
-    res.json(books);
+    // res.json(books);
+      res.render('index', { title: 'Books', books: books });
   } catch (error) {
     next(error);
   }
@@ -32,6 +34,67 @@ router.post('/books/new', async function(req, res, next) {
     next(error);
   }
 });
+
+
+
+router.get('/books/:id', async function(req, res, next) {
+  try {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      res.render('book-detail', { book: book });
+    } else {
+      const notFoundError = new Error('Book Not Found');
+      notFoundError.status = 404;
+      next(notFoundError);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* POST /books/:id - Updates book info in the database */
+router.post('/books/:id', async function(req, res, next) {
+  try {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.update(req.body);
+      res.redirect('/books');
+    } else {
+      const notFoundError = new Error('Book Not Found');
+      notFoundError.status = 404;
+      throw notFoundError;
+    }
+  } catch (error) {
+    if (error instanceof Sequelize.ValidationError) {
+      const book = Book.build(req.body);
+      book.id = req.params.id; // Ensure correct book.id is being passed
+      res.render('book-detail', { book: book, errors: error.errors });
+    } else {
+      next(error);
+    }
+  }
+});
+
+
+/* POST /books/:id/delete - Deletes a book */
+router.post('/books/:id/delete', async function(req, res, next) {
+  try {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.destroy();
+      res.redirect('/books');
+    } else {
+      const notFoundError = new Error('Book Not Found');
+      notFoundError.status = 404;
+      throw notFoundError;
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
 
 
 module.exports = router;
