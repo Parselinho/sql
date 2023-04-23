bd3a82bf5e973c5f76ff8236ab73f93c90f3e535
Gal Parselani <Parselinho@gmail.com>
2023-04-23 23:41:39 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const { Book } = require('../models'); // Import Book model
 const { Op } = require("sequelize");
 const { Sequelize } = require('sequelize');
+const BOOKS_PER_PAGE = 10;
 
 
 /* Redirect home page to /books */
@@ -11,16 +12,38 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET /books */
-router.get('/books', async function(req, res, next) {
+/* GET /books */
+router.get('/books', async function (req, res, next) {
   try {
-    const books = await Book.findAll();
-    // console.log(books);
-    // res.json(books);
-      res.render('index', { title: 'Books', books: books });
+    const search = req.query.search || '';
+    const page = parseInt(req.query.page) || 1;
+    const offset = (page - 1) * BOOKS_PER_PAGE;
+    const books = await Book.findAndCountAll({
+      where: {
+        [Op.or]: [
+          { title: { [Op.like]: `%${search}%` } },
+          { author: { [Op.like]: `%${search}%` } },
+          { genre: { [Op.like]: `%${search}%` } },
+          { year: { [Op.like]: `%${search}%` } },
+        ],
+      },
+      limit: BOOKS_PER_PAGE,
+      offset: offset,
+    });
+    const totalPages = Math.ceil(books.count / BOOKS_PER_PAGE);
+    res.render('index', {
+      title: 'Books',
+      books: books.rows,
+      search: search,
+      page: page,
+      totalPages: totalPages,
+    });
   } catch (error) {
     next(error);
   }
 });
+
+
 
 /* GET /books/new - Show the create new book form */
 router.get('/books/new', function(req, res, next) {
@@ -30,13 +53,13 @@ router.get('/books/new', function(req, res, next) {
 /* POST /books/new - Post a new book to the database */
 router.post('/books/new', async function(req, res, next) {
   try {
-    const newBook = await Book.create(req.body);
-    res.redirect('/books');
+    const newBook = await Book.create(req.body); // Create a new book in the database with the data from the request body
+    res.redirect('/books'); // Redirect the user to the '/books' page
   } catch (error) {
     if (error instanceof Sequelize.ValidationError) {
-      res.render('new-book', { errors: error.errors, book: req.body });
+      res.render('new-book', { errors: error.errors, book: req.body }); // Render the 'new-book' template with any validation errors that occurred
     } else {
-      next(error);
+      next(error); // Pass any other errors to the error handler middleware
     }
   }
 });
@@ -44,12 +67,12 @@ router.post('/books/new', async function(req, res, next) {
 
 
 
-
+/* GET /books/:id - Show the book details */
 router.get('/books/:id', async function(req, res, next) {
   try {
     const book = await Book.findByPk(req.params.id);
     if (book) {
-      res.render('book-detail', { book: book });
+      res.render('book-detail', { book: book }); // Render the 'book-detail' template with the retrieved book
     } else {
       const notFoundError = new Error('Book Not Found');
       notFoundError.status = 404;
@@ -63,22 +86,22 @@ router.get('/books/:id', async function(req, res, next) {
 /* POST /books/:id - Updates book info in the database */
 router.post('/books/:id', async function(req, res, next) {
   try {
-    const book = await Book.findByPk(req.params.id);
+    const book = await Book.findByPk(req.params.id); // Retrieve the book from the database by its primary key
     if (book) {
-      await book.update(req.body);
-      res.redirect('/books');
+      await book.update(req.body); // Update the book with the new information
+      res.redirect('/books'); // Redirect the user to the '/books' page
     } else {
       const notFoundError = new Error('Book Not Found');
       notFoundError.status = 404;
-      throw notFoundError;
+      throw notFoundError; // Throw a 404 error if the book isn't found
     }
   } catch (error) {
     if (error instanceof Sequelize.ValidationError) {
-      const book = Book.build(req.body);
+      const book = Book.build(req.body); // Create a new book object with the updated information
       book.id = req.params.id; // Ensure correct book.id is being passed
-      res.render('book-detail', { book: book, errors: error.errors });
+      res.render('book-detail', { book: book, errors: error.errors }); // Render the 'book-detail' template with any validation errors that occurred
     } else {
-      next(error);
+      next(error); // Pass any other errors to the error handler middleware
     }
   }
 });
@@ -103,14 +126,14 @@ router.get('/books/:id/edit', async function(req, res, next) {
 /* POST /books/:id/delete - Deletes a book */
 router.post('/books/:id/delete', async function(req, res, next) {
   try {
-    const book = await Book.findByPk(req.params.id);
+    const book = await Book.findByPk(req.params.id); // Retrieve the book from the database by its primary key
     if (book) {
-      await book.destroy();
-      res.redirect('/books');
+      await book.destroy(); // Delete the book from the database
+      res.redirect('/books'); // Redirect the user to the '/books' page
     } else {
       const notFoundError = new Error('Book Not Found');
       notFoundError.status = 404;
-      throw notFoundError;
+      throw notFoundError; 
     }
   } catch (error) {
     next(error);
